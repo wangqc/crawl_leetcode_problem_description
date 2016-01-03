384f0f3af942464f51a40c5f13f61d21eae2c032
wangqc <applefall@163.com>
2016-01-03 19:13:28 +0800

    succfully crawl one problem's description and save locally

--- claw_leetcode.py
+++ claw_leetcode.py
@@ -3,13 +3,49 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import string
+import os
 
-origin_url = "https://leetcode.com/"
-question_list_page = "problemset/algorithms"
+origin_url = "https://leetcode.com"
+question_list_page = "/problemset/algorithms"
 
 s = requests.Session()
+qs = requests.Session()
+
+def fetch_problem_content_and_write_to_file(child):
+    ex_url_tag = child.find("a")
+    ex_url = ex_url_tag['href']
+    ex_name = ex_url_tag.text               #题目名
+    ex_new_name = '-'.join(ex_name.split(' '))
+    td_list = child.find_all("td")
+    ex_num = td_list[1].text                #题目题号
+    ex_difficulty = td_list[4].text         #题目难度
+
+    qr = qs.get(origin_url + ex_url)
+    qsoup = BeautifulSoup(qr.text, "lxml")
+    qUntilDesc = qsoup.find_all("meta", limit = 3)
+    print qUntilDesc[2]['content']
+
+    exfilename = ex_num.zfill(3) + '-' + ex_new_name + ".txt"
+    if(ex_difficulty == "Easy"):
+        ex_output = open("./LEETCODE_EASY/" + exfilename, "w")
+    elif(ex_difficulty == "Medium"):
+        ex_output = open("./LEETCODE_MEDIUM/" + exfilename, "w")
+    elif(ex_difficulty == "Hard"):
+        ex_output = open("./LEETCODE_HARD/" + exfilename, "w")
+    ex_output.write(qUntilDesc[2]['content'])
+    ex_output.close()
+
+
+
 
 def fetch_problem_url():
+    if(not os.path.exists("./LEETCODE_EASY/")):
+        os.makedirs("./LEETCODE_EASY/")
+    if(not os.path.exists("./LEETCODE_MEDIUM/")):
+        os.makedirs("./LEETCODE_MEDIUM/")
+    if(not os.path.exists("./LEETCODE_HARD/")):
+        os.makedirs("./LEETCODE_HARD/")
+    
     r = s.get(origin_url + question_list_page)
     soup = BeautifulSoup(r.text, "lxml")
     pre_sibling = soup.find("div", id = "cong", class_ = "hide container")
@@ -18,8 +54,7 @@ def fetch_problem_url():
     only_once = True
     for child in ex_list.find_all("tr"):
         if(only_once):
-            ex_url = child.find("a")['href']
-            print ex_url
+            fetch_problem_content_and_write_to_file(child)
             only_once = False
 
 if __name__ == "__main__":
